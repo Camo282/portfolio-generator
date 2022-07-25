f93a5fa290d8a53c8d5e5513aaff4733b9d6c50e
Camo282 <camo282@gmail.com>
2022-07-25 15:02:49 -0400

    updated-template complete

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-//const fs = require('fs');
-//const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 
 const promptUser = () => {
@@ -128,9 +128,8 @@ promptUser()
   .then(promptProject)
   .then(portfolioData => {
     console.log(portfolioData);
-    //const pageHTML = generatePage(name, github);
-    //fs.writeFile('./index.html', pageHTML, err => {
-    // if (err) throw new Error(err);
-    //console.log('Portfolio complete! Check out index.html to see the output!');
-    //});
+    const pageHTML = generatePage(portfolioData);
+    fs.writeFile('./index.html', pageHTML, err => {
+     if (err) throw new Error(err);
+    });
   });
